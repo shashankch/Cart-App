14e401410ccc1e758caccaddaa4309560e330119
shashank singh chandel <shashakchandel@gmail.com>
2020-08-28 01:19:25 +0530

    add component state and styling

--- src/CartItem.js
+++ src/CartItem.js
@@ -1,17 +1,45 @@
 import React from 'react';
 
 class CartItem extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      price: 56000,
+      title: 'Mobile Phone',
+      qty: 1,
+      img: '',
+    };
+  }
+
   render() {
+    const { price, title, qty } = this.state;
     return (
       <div className='cart-item'>
         <div className='left-block'>
           <img style={styles.image} />
         </div>
         <div className='right-block'>
-          <div style={{ fontSize: 25 }}>Mobile</div>
-          <div style={{ color: '#777' }}>INR 56000</div>
-          <div style={{ color: '#777' }}>Qty: 1</div>
-          <div className='cart-item-actions'>{/* buttons */}</div>
+          <div style={{ fontSize: 25 }}>{title}</div>
+          <div style={{ color: '#777' }}>INR {price}</div>
+          <div style={{ color: '#777' }}>Qty: {qty}</div>
+          <div className='cart-item-actions'>
+            {/* buttons */}
+            <img
+              alt='increase'
+              className='action-icons'
+              src='https://image.flaticon.com/icons/svg/864/864378.svg'
+            />
+            <img
+              alt='decrease'
+              className='action-icons'
+              src='https://image.flaticon.com/icons/svg/864/864373.svg'
+            />
+            <img
+              alt='delete'
+              className='action-icons'
+              src='https://image.flaticon.com/icons/svg/1214/1214428.svg'
+            />
+          </div>
         </div>
       </div>
     );
@@ -22,8 +50,8 @@ const styles = {
   image: {
     height: 110,
     width: 110,
-        borderRadius: 4,
-    background:'#ccc'
+    borderRadius: 4,
+    background: '#ccc',
   },
 };
 
