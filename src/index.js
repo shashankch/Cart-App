ad296e2a50087696ce5148c0ac62698f4896acaf
shashank singh chandel <shashakchandel@gmail.com>
2020-08-30 18:06:56 +0530

    update

--- src/index.js
+++ src/index.js
@@ -10,13 +10,13 @@ import 'firebase/firestore';
 
 
   var firebaseConfig = {
-    apiKey: "AIzaSyBsJt2z0SfItNrycKwdIk46U6BBy0v94D0",
-    authDomain: "cart-4866d.firebaseapp.com",
-    databaseURL: "https://cart-4866d.firebaseio.com",
+    apiKey: "",
+    authDomain: "",
+    databaseURL: "",
     projectId: "cart-4866d",
-    storageBucket: "cart-4866d.appspot.com",
-    messagingSenderId: "203828368207",
-    appId: "1:203828368207:web:84146ffab74d970bcbeb3a"
+    storageBucket: "",
+    messagingSenderId: "",
+    appId: ""
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
